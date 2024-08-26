9b0c8ff40cf83429b3a700327f01ca69bad85f3c
Arthur Cruz <developer.arthur.cruz@gmail.com>
2024-08-26 00:26:54 -0300

    FEAT[Locations]: Implements Locations creation flux

--- app/dashboard/locations/locationSlice.ts
+++ app/dashboard/locations/locationSlice.ts
@@ -1,9 +1,13 @@
 import ApiClient from "@/lib/api_client";
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
 import { access, stat } from "fs";
 
 export interface Location {
-  id: number;
+  id?: number;
   name: string;
   user_id: number;
 }
@@ -33,9 +37,9 @@ export const doGetAllLocations = createAsyncThunk(
     return response.data;
   }
 );
-//https://stackoverflow.com/questions/76987888/how-can-i-group-redux-store-slices-under-the-same-property
-export const locationsSlice = createSlice({
-  name: "locations",
+
+export const getAllLocationsSlice = createSlice({
+  name: "getAllLocations",
   initialState: initialGetAllLocationsState,
   reducers: {},
   extraReducers(builder) {
@@ -60,4 +64,59 @@ export const locationsSlice = createSlice({
   },
 });
 
-export default locationsSlice.reducer;
+export interface CreateLocationState {
+  status: "idle" | "loading" | "success" | "failure";
+  error: boolean;
+  errorMsg: string | undefined;
+  location?: Location;
+}
+
+const initialCreateLocationState: CreateLocationState = {
+  status: "idle",
+  error: false,
+  errorMsg: undefined,
+  location: undefined,
+};
+
+export const doCreateLocation = createAsyncThunk(
+  "locations/",
+  async (location: Location) => {
+    const response = await ApiClient.post("/v1/locations", location).catch(
+      function (error) {
+        throw new Error(error.response.data.msg);
+      }
+    );
+    return response.data;
+  }
+);
+
+export const createLocationsSlice = createSlice({
+  name: "createLocations",
+  initialState: initialCreateLocationState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(doCreateLocation.pending, (state, _) => {
+        state.status = "loading";
+        state.error = false;
+        state.errorMsg = undefined;
+      })
+      .addCase(doCreateLocation.fulfilled, (state, action) => {
+        state.status = "success";
+        state.error = false;
+        state.errorMsg = undefined;
+        state.location = action.payload.location as Location;
+      })
+      .addCase(doCreateLocation.rejected, (state, action) => {
+        state.status = "failure";
+        state.error = true;
+        state.errorMsg = action.error.message;
+        state.location = undefined;
+      });
+  },
+});
+
+export default combineReducers({
+  getAllLocationsSlice: getAllLocationsSlice.reducer,
+  createLocationsSlice: createLocationsSlice.reducer,
+});
--- app/dashboard/locations/page.tsx
+++ app/dashboard/locations/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { doGetAllLocations, Location } from "./locationSlice";
+import { doCreateLocation, doGetAllLocations, Location } from "./locationSlice";
 import { Button } from "@/components/ui/button";
-import { Edit2, Plus, Search, Trash2 } from "lucide-react";
+import { Edit2, Loader2, Plus, Search, Trash2 } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -14,11 +14,36 @@ import styles from "./styles.module.css";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import debounce from "lodash.debounce";
+import { Label } from "@/components/ui/label";
+import {
+  DialogHeader,
+  DialogFooter,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { getTokenData } from "@/lib/api_client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function LocationsPage() {
   const dispatch = useAppDispatch();
-  const getAllLocationsState = useAppSelector((state) => state.locations);
+  const getAllLocationsState = useAppSelector(
+    (state) => state.locations.getAllLocationsSlice
+  );
   const [search, setSearch] = useState("");
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -33,10 +58,13 @@ export default function LocationsPage() {
   });
 
   useEffect(() => {
-    if (getAllLocationsState.status === "idle") {
+    if (
+      getAllLocationsState.status === "idle" ||
+      getAllLocationsState.status === "success"
+    ) {
       dispatch(doGetAllLocations());
     }
-  }, [dispatch, getAllLocationsState.status]);
+  }, []);
 
   const getLocations = useCallback(() => {
     return getAllLocationsState.locations
@@ -55,9 +83,23 @@ export default function LocationsPage() {
     <main className="flex w-full h-full flex-col justify-center items-center">
       <div className="w-full flex justify-start items-center mb-7">
         <h1 className="font-serif font-semibold text-3xl">Localizações</h1>
-        <Button variant="default" size={"sm"} className="ml-5">
-          <Plus />
-        </Button>
+
+        <Dialog open={dialogOpen}>
+          <DialogTrigger>
+            <Button
+              variant="default"
+              size={"sm"}
+              className="ml-5"
+              onClick={() => setDialogOpen(true)}
+            >
+              <Plus />
+            </Button>
+          </DialogTrigger>
+          <AddEditLocation
+            location={undefined}
+            closeCallback={() => setDialogOpen(false)}
+          />
+        </Dialog>
         <div className="flex-grow"></div>
         <div className="flex w-full max-w-sm items-center space-x-2">
           <Search className="relative left-9 text-gray-500" size={"24px"} />
@@ -83,6 +125,13 @@ export default function LocationsPage() {
           getAllLocationsState.status != null &&
           getAllLocationsState.status === "success" &&
           getLocations()}
+        {getAllLocationsState != null &&
+          getAllLocationsState.status != null &&
+          getAllLocationsState.status === "loading" && (
+            <div className="flex w-full h-full justify-center items-center">
+              <Loader2 className="mt-16 h-20 w-20 animate-spin" />
+            </div>
+          )}
       </div>
     </main>
   );
@@ -121,5 +170,101 @@ function LocationComponent({
         </Button>
       </CardFooter>
     </Card>
+  );
+}
+
+type AddEditLocationProps = {
+  location?: Location;
+  closeCallback: Function;
+};
+
+export function AddEditLocation({
+  location,
+  closeCallback,
+}: AddEditLocationProps): React.ReactElement {
+  const [locationName, setLocationName] = useState("");
+  const [userId, setUserId] = useState(0);
+
+  const dispatch = useAppDispatch();
+  const createLocationState = useAppSelector(
+    (state) => state.locations.createLocationsSlice
+  );
+
+  useEffect(() => {
+    setUserId(getTokenData()?.id ?? 0);
+  }, []);
+
+  const handleSubmitLocation = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
+
+  const formSchema = z.object({
+    name: z.string().min(1).max(255),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    let newLocation: Location = {
+      id: location?.id,
+      name: values.name,
+      user_id: location?.user_id ?? userId,
+    };
+    if (location?.id != undefined) {
+      //TODO
+    } else {
+      dispatch(doCreateLocation(newLocation));
+      closeCallback(true);
+    }
+  }
+
+  useEffect(() => {
+    if (createLocationState.status === "success") {
+      dispatch(doGetAllLocations());
+    }
+  }, [dispatch, createLocationState.status]);
+
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>
+          {location?.id == null ? "Criar" : "Editar"} Localização
+        </DialogTitle>
+      </DialogHeader>
+      {(createLocationState?.status == "idle" ||
+        createLocationState.status == "success") && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="locationName"
+                      value={locationName}
+                      onChange={(e) => setLocationName(e.target.value)}
+                      className="col-span-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">
+              {location?.id == null ? "Criar" : "Editar"}
+            </Button>
+          </form>
+        </Form>
+      )}
+      <DialogFooter></DialogFooter>
+    </DialogContent>
   );
 }
