bfe6bef22114ebdeb90482dc8be4781e4d0d9244
Arthur Cruz <developer.arthur.cruz@gmail.com>
2026-04-05 10:14:07 -0300

    FEAT[Project]: Updated dependendcies and Dockerfile

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
 import StoreProvider from "../storeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
